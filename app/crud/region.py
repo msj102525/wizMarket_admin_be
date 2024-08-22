619044594c253e50d919a600be719b1a4ff0dc34
DoorWinBell <msj102525@gmail.com>
2024-08-22 10:51:53 +0900

    region id 함수 수정 - 문승종 0822

--- app/crud/region.py
+++ app/crud/region.py
@@ -8,23 +8,32 @@ from app.db.connect import (
 )
 
 
-def get_region_id(city: str, district: str, sub_district: str):
+def get_or_create_region_id(city: str, district: str, sub_district: str):
     connection = get_db_connection()
     cursor = None
     try:
         if connection.open:
             cursor = connection.cursor()
-            sql = """
+
+            select_sql = """
                 SELECT REGION_ID FROM REGION 
                 WHERE CITY = %s AND DISTRICT = %s AND SUB_DISTRICT = %s
             """
-            cursor.execute(sql, (city, district, sub_district))
+            cursor.execute(select_sql, (city, district, sub_district))
             result = cursor.fetchone()
 
             if result:
                 return result[0]
             else:
-                return None
+                insert_sql = """
+                    INSERT INTO REGION (CITY, DISTRICT, SUB_DISTRICT)
+                    VALUES (%s, %s, %s)
+                """
+                cursor.execute(insert_sql, (city, district, sub_district))
+                region_id = cursor.lastrowid
+
+                commit(connection)
+                return region_id
 
     except pymysql.MySQLError as e:
         print(f"Database query failed: {e}")
