59c01fe410c2087525a105bdf20ca9a28949a127
DoorWinBell <msj102525@gmail.com>
2024-09-05 17:18:03 +0900

    app/api/endpoints/city.py conflict 해결 - 문승종 0905

--- app/api/endpoints/city.py
+++ app/api/endpoints/city.py
@@ -6,22 +6,9 @@ from app.service.city import get_locations_service
 
 router = APIRouter()
 
+
 @router.get("/locations")
 async def get_locations():
-    locations = await get_locations_service()  # 서비스 호출
+    locations = await get_locations_service() 
     print(locations)
     return locations
-from fastapi import APIRouter, HTTPException
-from typing import List
-
-
-router = APIRouter()
-
-
-@router.get("", response_model=List[None])
-def get_city_list(city_id: int):
-    print(f"city_id: {city_id}")
-    try:
-        return None
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
