c50695719eccb075b03ef094240163c6dde52937
DoorWinBell <msj102525@gmail.com>
2024-10-15 11:06:03 +0900

    프로젝트 중간 점검 - 문승종 20241015

--- app/api/endpoints/report.py
+++ app/api/endpoints/report.py
@@ -42,10 +42,10 @@ from app.service.statistics import (
     select_statistics_by_store_business_number as service_select_statistics_by_store_business_number,
 )
 
-from app.service.gpt_generate import(
+from app.service.gpt_generate import (
     report_loc_info,
     report_rising_menu,
-    report_today_tip
+    report_today_tip,
 )
 from fastapi.responses import PlainTextResponse
 
@@ -177,13 +177,23 @@ def select_population_compare_resident_work(store_business_id: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"{e}Internal Server Error")
 
+
 # CommercialDistrictSummary
 @router.get("/gpt/report_loc_info", response_model=GPTReport)
 def generate_report_loc_info_from_gpt(store_business_id: str):
     # print(store_business_id)
     try:
-        report_content = report_loc_info(store_business_id)
-        report = PlainTextResponse(report_content)
+        # report_content = report_loc_info(store_business_id)
+        # report = PlainTextResponse(report_content)
+        report = PlainTextResponse(
+            """Dummy Daya:
+            3.5식당은 서울특별시 영등포구 신길5동에 위치한 백반/한정식 전문 음식점입니다.
+            해당 지역은 업소 수가 많지 않으며(약 3.3점), 상대적으로 높은 평균 매출(약 6.9점)과
+            중간 수준의 평균소득(약 6.2점)을 보입니다. 월 평균소비 능력과 유동인구는 평균 이하인 반면,
+            주거 인구와 세대 수는 중간 수준으로 안정적인 성향을 보입니다. 목표 고객으로는 30대에서 60대
+            이상이 주로 많은 연령대임을 고려하여 중장년층을 타깃으로 삼는 것이 유리할 것입니다.
+            운영 가이드로는 주거 인구 중심의 마케팅과 인근 거주민의 생활 패턴에 맞춘 점심 및 저녁 메뉴 특화 전략을 제안드립니다."""
+        )
         return report
 
     except HTTPException as http_ex:
@@ -193,27 +203,35 @@ def generate_report_loc_info_from_gpt(store_business_id: str):
         print(f"Unhandled exception: {e}")
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
+
 # RisingMenu
 @router.get("/gpt/report_rising_menu", response_model=GPTReport)
 def generate_report_rising_menu_from_gpt(store_business_id: str):
     # print(store_business_id)
     try:
-        report_content = report_rising_menu(store_business_id)
-        report = PlainTextResponse(report_content)
+        # report_content = report_rising_menu(store_business_id)
+        # report = PlainTextResponse(report_content)
+        report = PlainTextResponse(
+            """Dummy Data 신길5동에서 백반집 운영하신다구요?
+        좋습니다, 백반이랑 돼지고기, 소주가 뜬다니 집중할 포인트가 생겼네요!
+        첫째, '백반' 메뉴를 다양하게 준비하셔서 매일 다른 반찬으로 고객들 기대치를 높이세요.
+        둘째, 돼지고기구이랑 볶음을 잘 준비해, 식사와 술 한 잔 곁들이기 좋은 메뉴로 만드시면 손님이 자주 찾을 겁니다.
+        마지막으로는, 소고기구이는 약간의 고급 메뉴로 포지셔닝해서 가족이나 친구들끼리 모임 장소로 만들면 좋겠어요. 화이팅입니다!"""
+        )
         return report
 
     except HTTPException as http_ex:
         raise http_ex
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"{e}Internal Server Error")
-    
+
 
 @router.get("/gpt/report_today_tip", response_model=GPTReport)
 def generate_report_today_tip_from_gpt(store_business_id: str):
     # print(store_business_id)
     try:
-        weather= get_report_store_info(store_business_id)
-        tmp = weather.weatherInfo 
+        weather = get_report_store_info(store_business_id)
+        tmp = weather.weatherInfo
         temp = tmp.temp
         report = report_today_tip(store_business_id, temp)
 
