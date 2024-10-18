60aeef2ede586f7cb17db7c9a741cd6badb2b4ff
DoorWinBell <msj102525@gmail.com>
2024-10-18 17:55:24 +0900

    상권분석 통계 주석 처리 - 문승종 20241018

--- app/api/endpoints/commercial_district.py
+++ app/api/endpoints/commercial_district.py
@@ -172,81 +172,81 @@ def get_commercial_district_by_query(
 
 
 # @router.get("/cd3", response_model=List[CommercialStatisticsOutput])
-@router.get("/cd3", response_model=List[CommercialStatisticsOutput])
-def get_commercial_district_by_query(
-    city_id: Optional[int] = Query(None),
-    district_id: Optional[int] = Query(None),
-    sub_district_id: Optional[int] = Query(None),
-    biz_main_category_id: Optional[int] = Query(None),
-    biz_sub_category_id: Optional[int] = Query(None),
-    biz_detail_category_id: Optional[int] = Query(None),
-    market_size_min: Optional[int] = Query(None),
-    market_size_max: Optional[int] = Query(None),
-    avg_sales_min: Optional[int] = Query(None),
-    avg_sales_max: Optional[int] = Query(None),
-    operating_cost_min: Optional[int] = Query(None),
-    operating_cost_max: Optional[int] = Query(None),
-    food_cost_min: Optional[int] = Query(None),
-    food_cost_max: Optional[int] = Query(None),
-    employee_cost_min: Optional[int] = Query(None),  # 인건비X -> 평균 결제
-    employee_cost_max: Optional[int] = Query(None),  # 인건비X -> 평균 결제
-    rental_cost_min: Optional[int] = Query(None),
-    rental_cost_max: Optional[int] = Query(None),
-    avg_profit_min: Optional[int] = Query(None),
-    avg_profit_max: Optional[int] = Query(None),
-):
-    try:
-        # 동적 쿼리로 상권 데이터 가져오기
-        results: CommercialDistrictOutput = (
-            service_select_commercial_district_by_dynamic_query(
-                city_id,
-                district_id,
-                sub_district_id,
-                biz_main_category_id,
-                biz_sub_category_id,
-                biz_detail_category_id,
-                market_size_min,
-                market_size_max,
-                avg_sales_min,
-                avg_sales_max,
-                operating_cost_min,
-                operating_cost_max,
-                food_cost_min,
-                food_cost_max,
-                employee_cost_min,
-                employee_cost_max,
-                rental_cost_min,
-                rental_cost_max,
-                avg_profit_min,
-                avg_profit_max,
-            )
-        )
+# @router.get("/cd3", response_model=List[CommercialStatisticsOutput])
+# def get_commercial_district_by_query(
+#     city_id: Optional[int] = Query(None),
+#     district_id: Optional[int] = Query(None),
+#     sub_district_id: Optional[int] = Query(None),
+#     biz_main_category_id: Optional[int] = Query(None),
+#     biz_sub_category_id: Optional[int] = Query(None),
+#     biz_detail_category_id: Optional[int] = Query(None),
+#     market_size_min: Optional[int] = Query(None),
+#     market_size_max: Optional[int] = Query(None),
+#     avg_sales_min: Optional[int] = Query(None),
+#     avg_sales_max: Optional[int] = Query(None),
+#     operating_cost_min: Optional[int] = Query(None),
+#     operating_cost_max: Optional[int] = Query(None),
+#     food_cost_min: Optional[int] = Query(None),
+#     food_cost_max: Optional[int] = Query(None),
+#     employee_cost_min: Optional[int] = Query(None),  # 인건비X -> 평균 결제
+#     employee_cost_max: Optional[int] = Query(None),  # 인건비X -> 평균 결제
+#     rental_cost_min: Optional[int] = Query(None),
+#     rental_cost_max: Optional[int] = Query(None),
+#     avg_profit_min: Optional[int] = Query(None),
+#     avg_profit_max: Optional[int] = Query(None),
+# ):
+#     try:
+#         # 동적 쿼리로 상권 데이터 가져오기
+#         results: CommercialDistrictOutput = (
+#             service_select_commercial_district_by_dynamic_query(
+#                 city_id,
+#                 district_id,
+#                 sub_district_id,
+#                 biz_main_category_id,
+#                 biz_sub_category_id,
+#                 biz_detail_category_id,
+#                 market_size_min,
+#                 market_size_max,
+#                 avg_sales_min,
+#                 avg_sales_max,
+#                 operating_cost_min,
+#                 operating_cost_max,
+#                 food_cost_min,
+#                 food_cost_max,
+#                 employee_cost_min,
+#                 employee_cost_max,
+#                 rental_cost_min,
+#                 rental_cost_max,
+#                 avg_profit_min,
+#                 avg_profit_max,
+#             )
+#         )
 
-        # 결과와 통계 데이터를 결합하여 반환할 리스트
-        combined_results = []
+#         # 결과와 통계 데이터를 결합하여 반환할 리스트
+#         combined_results = []
 
-        for item in results:
-            # 각 상권의 통계 데이터 가져오기
-            statistics_data: CommercialStatisticsData = (
-                service_select_statistics_by_sub_district_detail_category_new(
-                    item.city_name,
-                    item.district_name,
-                    item.sub_district_name,
-                    item.biz_detail_category_name,
-                )
-            )
+#         for item in results:
+#             # 각 상권의 통계 데이터 가져오기
+#             statistics_data: CommercialStatisticsData = (
+#                 service_select_statistics_by_sub_district_detail_category_new(
+#                     item.city_name,
+#                     item.district_name,
+#                     item.sub_district_name,
+#                     item.biz_detail_category_name,
+#                 )
+#             )
 
-            # 결과와 통계 데이터를 결합
-            combined_results.append(
-                CommercialStatisticsOutput(
-                    commercial_district_data=item,
-                    statistics_data=statistics_data,
-                )
-            )
+#             # 결과와 통계 데이터를 결합
+#             combined_results.append(
+#                 CommercialStatisticsOutput(
+#                     commercial_district_data=item,
+#                     statistics_data=statistics_data,
+#                 )
+#             )
 
-        return combined_results
+#         return combined_results
 
-    except HTTPException as http_ex:
-        raise http_ex
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+#     except HTTPException as http_ex:
+#         raise http_ex
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
