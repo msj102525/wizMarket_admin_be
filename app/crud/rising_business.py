4a1c6ec67b90752f89aef84efea731f6ebb67d01
DoorWinBell <msj102525@gmail.com>
2024-11-12 11:25:11 +0900

    뜨는 업종 리스트 쿼리 수정 - 문승종 20241112

--- app/crud/rising_business.py
+++ app/crud/rising_business.py
@@ -1,3 +1,4 @@
+from datetime import date
 import logging
 from pymysql import MySQLError
 from typing import List, Optional
@@ -149,6 +150,137 @@ def select_all_rising_business_by_region_id(
     return results
 
 
+# def select_all_rising_business_by_dynamic_query(
+#     city_id: Optional[int] = None,
+#     district_id: Optional[int] = None,
+#     sub_district_id: Optional[int] = None,
+#     biz_main_category_id: Optional[int] = None,
+#     biz_sub_category_id: Optional[int] = None,
+#     biz_detail_category_id: Optional[int] = None,
+#     growth_rate_min: Optional[float] = None,
+#     growth_rate_max: Optional[float] = None,
+#     rank_min: Optional[int] = None,
+#     rank_max: Optional[int] = None,
+# ) -> List[RisingBusinessOutput]:
+#     connection = get_db_connection()
+#     cursor = connection.cursor(pymysql.cursors.DictCursor)
+#     logger = logging.getLogger(__name__)
+#     results: List[RisingBusinessOutput] = []
+
+#     try:
+#         if connection.open:
+#             select_query = """
+#                 SELECT
+#                     rb.RISING_BUSINESS_ID,
+#                     CI.CITY_NAME,
+#                     DI.DISTRICT_NAME,
+#                     SD.SUB_DISTRICT_NAME,
+#                     BMC.BIZ_MAIN_CATEGORY_NAME,
+#                     BSC.BIZ_SUB_CATEGORY_NAME,
+#                     BDC.BIZ_DETAIL_CATEGORY_NAME,
+#                     rb.growth_rate,
+#                     rb.sub_district_rank,
+#                     rb.y_m,
+#                     rb.CREATED_AT,
+#                     rb.UPDATED_AT
+#                 FROM
+#                     rising_business rb
+#                 JOIN
+#                     CITY CI ON rb.CITY_ID = CI.CITY_ID
+#                 JOIN
+#                     DISTRICT DI ON rb.DISTRICT_ID = DI.DISTRICT_ID
+#                 JOIN
+#                     SUB_DISTRICT SD ON rb.SUB_DISTRICT_ID = SD.SUB_DISTRICT_ID
+#                 JOIN
+#                     BIZ_MAIN_CATEGORY BMC ON rb.BIZ_MAIN_CATEGORY_ID = BMC.BIZ_MAIN_CATEGORY_ID
+#                 JOIN
+#                     BIZ_SUB_CATEGORY BSC ON rb.BIZ_SUB_CATEGORY_ID = BSC.BIZ_SUB_CATEGORY_ID
+#                 JOIN
+#                     BIZ_DETAIL_CATEGORY BDC ON rb.BIZ_DETAIL_CATEGORY_ID = BDC.BIZ_DETAIL_CATEGORY_ID
+#                 WHERE 1=1 AND BDC.BIZ_DETAIL_CATEGORY_NAME != '소분류없음'
+#             """
+
+#             # 조건을 동적으로 추가
+#             params = []
+#             if city_id is not None:
+#                 select_query += " AND rb.CITY_ID = %s"
+#                 params.append(city_id)
+#             if district_id is not None:
+#                 select_query += " AND rb.DISTRICT_ID = %s"
+#                 params.append(district_id)
+#             if sub_district_id is not None:
+#                 select_query += " AND rb.SUB_DISTRICT_ID = %s"
+#                 params.append(sub_district_id)
+#             if biz_main_category_id is not None:
+#                 select_query += " AND rb.BIZ_MAIN_CATEGORY_ID = %s"
+#                 params.append(biz_main_category_id)
+#             if biz_sub_category_id is not None:
+#                 select_query += " AND rb.BIZ_SUB_CATEGORY_ID = %s"
+#                 params.append(biz_sub_category_id)
+#             if biz_detail_category_id is not None:
+#                 select_query += " AND rb.BIZ_DETAIL_CATEGORY_ID = %s"
+#                 params.append(biz_detail_category_id)
+#             if growth_rate_min is not None:
+#                 select_query += " AND rb.growth_rate >= %s"
+#                 params.append(growth_rate_min)
+#             if growth_rate_max is not None:
+#                 select_query += " AND rb.growth_rate <= %s"
+#                 params.append(growth_rate_max)
+#             if rank_min is not None:
+#                 select_query += " AND rb.sub_district_rank >= %s"
+#                 params.append(rank_min)
+#             if rank_max is not None:
+#                 select_query += " AND rb.sub_district_rank <= %s"
+#                 params.append(rank_max)
+
+#             select_query += " ORDER BY rb.RISING_BUSINESS_ID DESC"
+
+#             # logger.info(f"Executing query: {select_query % tuple(params)}")
+#             cursor.execute(select_query, tuple(params))
+
+#             rows = cursor.fetchall()
+
+#             for row in rows:
+#                 rising_business_ouput = RisingBusinessOutput(
+#                     rising_business_id=row.get("RISING_BUSINESS_ID"),
+#                     city_name=row.get("CITY_NAME"),
+#                     district_name=row.get("DISTRICT_NAME"),
+#                     sub_district_name=row.get("SUB_DISTRICT_NAME"),
+#                     biz_main_category_name=row.get("BIZ_MAIN_CATEGORY_NAME"),
+#                     biz_sub_category_name=row.get("BIZ_SUB_CATEGORY_NAME"),
+#                     biz_detail_category_name=row.get("BIZ_DETAIL_CATEGORY_NAME"),
+#                     growth_rate=(
+#                         row.get("growth_rate")
+#                         if row.get("growth_rate") is not None
+#                         else 0.0
+#                     ),
+#                     sub_district_rank=(
+#                         row.get("sub_district_rank")
+#                         if row.get("sub_district_rank") is not None
+#                         else 0
+#                     ),
+#                     y_m=row.get("y_m"),
+#                     created_at=row.get("CREATED_AT"),
+#                     updated_at=row.get("UPDATED_AT"),
+#                 )
+#                 results.append(rising_business_ouput)
+
+#             return results
+#     except pymysql.MySQLError as e:
+#         logger.error(f"MySQL Error: {e}")
+#         rollback(connection)
+#     except Exception as e:
+#         logger.error(f"Unexpected Error: {e}")
+#         rollback(connection)
+#     finally:
+#         if cursor:
+#             close_cursor(cursor)
+#         if connection:
+#             close_connection(connection)
+
+#     return results
+
+
 def select_all_rising_business_by_dynamic_query(
     city_id: Optional[int] = None,
     district_id: Optional[int] = None,
@@ -160,6 +292,7 @@ def select_all_rising_business_by_dynamic_query(
     growth_rate_max: Optional[float] = None,
     rank_min: Optional[int] = None,
     rank_max: Optional[int] = None,
+    y_m: Optional[date] = None,
 ) -> List[RisingBusinessOutput]:
     connection = get_db_connection()
     cursor = connection.cursor(pymysql.cursors.DictCursor)
@@ -168,74 +301,62 @@ def select_all_rising_business_by_dynamic_query(
 
     try:
         if connection.open:
+            params = [
+                city_id,
+                district_id,
+                sub_district_id,
+                biz_main_category_id,
+                biz_sub_category_id,
+                biz_detail_category_id,
+                growth_rate_min,
+                growth_rate_max,
+                rank_min,
+                rank_max,
+                y_m,
+            ]
             select_query = """
-                SELECT
-                    rb.RISING_BUSINESS_ID,
-                    CI.CITY_NAME,
-                    DI.DISTRICT_NAME,
-                    SD.SUB_DISTRICT_NAME,
-                    BMC.BIZ_MAIN_CATEGORY_NAME,
-                    BSC.BIZ_SUB_CATEGORY_NAME,
-                    BDC.BIZ_DETAIL_CATEGORY_NAME,
-                    rb.growth_rate,
-                    rb.sub_district_rank,
-                    rb.y_m,
-                    rb.CREATED_AT,
-                    rb.UPDATED_AT
-                FROM
-                    rising_business rb
-                JOIN
-                    CITY CI ON rb.CITY_ID = CI.CITY_ID
-                JOIN
-                    DISTRICT DI ON rb.DISTRICT_ID = DI.DISTRICT_ID
-                JOIN
-                    SUB_DISTRICT SD ON rb.SUB_DISTRICT_ID = SD.SUB_DISTRICT_ID
-                JOIN
-                    BIZ_MAIN_CATEGORY BMC ON rb.BIZ_MAIN_CATEGORY_ID = BMC.BIZ_MAIN_CATEGORY_ID
-                JOIN
-                    BIZ_SUB_CATEGORY BSC ON rb.BIZ_SUB_CATEGORY_ID = BSC.BIZ_SUB_CATEGORY_ID
-                JOIN
-                    BIZ_DETAIL_CATEGORY BDC ON rb.BIZ_DETAIL_CATEGORY_ID = BDC.BIZ_DETAIL_CATEGORY_ID
-                WHERE 1=1 AND BDC.BIZ_DETAIL_CATEGORY_NAME != '소분류없음'
-            """
-
-            # 조건을 동적으로 추가
-            params = []
-            if city_id is not None:
-                select_query += " AND rb.CITY_ID = %s"
-                params.append(city_id)
-            if district_id is not None:
-                select_query += " AND rb.DISTRICT_ID = %s"
-                params.append(district_id)
-            if sub_district_id is not None:
-                select_query += " AND rb.SUB_DISTRICT_ID = %s"
-                params.append(sub_district_id)
-            if biz_main_category_id is not None:
-                select_query += " AND rb.BIZ_MAIN_CATEGORY_ID = %s"
-                params.append(biz_main_category_id)
-            if biz_sub_category_id is not None:
-                select_query += " AND rb.BIZ_SUB_CATEGORY_ID = %s"
-                params.append(biz_sub_category_id)
-            if biz_detail_category_id is not None:
-                select_query += " AND rb.BIZ_DETAIL_CATEGORY_ID = %s"
-                params.append(biz_detail_category_id)
-            if growth_rate_min is not None:
-                select_query += " AND rb.growth_rate >= %s"
-                params.append(growth_rate_min)
-            if growth_rate_max is not None:
-                select_query += " AND rb.growth_rate <= %s"
-                params.append(growth_rate_max)
-            if rank_min is not None:
-                select_query += " AND rb.sub_district_rank >= %s"
-                params.append(rank_min)
-            if rank_max is not None:
-                select_query += " AND rb.sub_district_rank <= %s"
-                params.append(rank_max)
-
-            select_query += " ORDER BY rb.RISING_BUSINESS_ID DESC"
+            WITH FilteredRB as (
+                SELECT *
+                FROM RISING_BUSINESS RB
+                WHERE 1=1
+                    AND RB.CITY_ID = COALESCE(%s, RB.CITY_ID)
+                    AND RB.DISTRICT_ID = COALESCE(%s, RB.DISTRICT_ID)
+                    AND RB.SUB_DISTRICT_ID = COALESCE(%s, RB.SUB_DISTRICT_ID)
+                    AND RB.BIZ_MAIN_CATEGORY_ID = COALESCE(%s, RB.BIZ_MAIN_CATEGORY_ID)
+                    AND RB.BIZ_SUB_CATEGORY_ID = COALESCE(%s, RB.BIZ_SUB_CATEGORY_ID)
+                    AND RB.BIZ_DETAIL_CATEGORY_ID = COALESCE(%s, RB.BIZ_DETAIL_CATEGORY_ID)
+                    AND RB.GROWTH_RATE BETWEEN COALESCE(%s, RB.GROWTH_RATE) AND COALESCE(%s, RB.GROWTH_RATE)
+                    AND RB.SUB_DISTRICT_RANK BETWEEN COALESCE(%s, RB.SUB_DISTRICT_RANK) AND COALESCE(%s, RB.SUB_DISTRICT_RANK)
+                    AND RB.Y_M = COALESCE(%s, RB.Y_M)
+            )
+            SELECT
+                RB.*,
+                CI.CITY_NAME,
+                DI.DISTRICT_NAME,
+                SD.SUB_DISTRICT_NAME,
+                BMC.BIZ_MAIN_CATEGORY_NAME,
+                BSC.BIZ_SUB_CATEGORY_NAME,
+                BDC.BIZ_DETAIL_CATEGORY_NAME
+            FROM
+                FilteredRB RB
+            JOIN
+                CITY CI ON RB.CITY_ID = CI.CITY_ID
+            JOIN
+                DISTRICT DI ON RB.DISTRICT_ID = DI.DISTRICT_ID
+            JOIN
+                SUB_DISTRICT SD ON RB.SUB_DISTRICT_ID = SD.SUB_DISTRICT_ID
+            JOIN
+                BIZ_MAIN_CATEGORY BMC ON RB.BIZ_MAIN_CATEGORY_ID = BMC.BIZ_MAIN_CATEGORY_ID
+            JOIN
+                BIZ_SUB_CATEGORY BSC ON RB.BIZ_SUB_CATEGORY_ID = BSC.BIZ_SUB_CATEGORY_ID
+            JOIN
+                BIZ_DETAIL_CATEGORY BDC ON RB.BIZ_DETAIL_CATEGORY_ID = BDC.BIZ_DETAIL_CATEGORY_ID
+            AND BDC.BIZ_DETAIL_CATEGORY_NAME != '소분류없음'
+            ORDER BY RB.RISING_BUSINESS_ID DESC
+        """
 
             # logger.info(f"Executing query: {select_query % tuple(params)}")
-            cursor.execute(select_query, tuple(params))
+            cursor.execute(select_query, params)
 
             rows = cursor.fetchall()
 
@@ -249,16 +370,16 @@ def select_all_rising_business_by_dynamic_query(
                     biz_sub_category_name=row.get("BIZ_SUB_CATEGORY_NAME"),
                     biz_detail_category_name=row.get("BIZ_DETAIL_CATEGORY_NAME"),
                     growth_rate=(
-                        row.get("growth_rate")
-                        if row.get("growth_rate") is not None
+                        row.get("GROWTH_RATE")
+                        if row.get("GROWTH_RATE") is not None
                         else 0.0
                     ),
                     sub_district_rank=(
-                        row.get("sub_district_rank")
-                        if row.get("sub_district_rank") is not None
+                        row.get("SUB_DISTRICT_RANK")
+                        if row.get("SUB_DISTRICT_RANK") is not None
                         else 0
                     ),
-                    y_m=row.get("y_m"),
+                    y_m=row.get("Y_M"),
                     created_at=row.get("CREATED_AT"),
                     updated_at=row.get("UPDATED_AT"),
                 )
